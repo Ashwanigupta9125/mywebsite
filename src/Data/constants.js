7ff8e0f31c4c1f522ab479c5a49d9f32b9229fcf
Ashwanigupta9125 <ashwanigameup@gmail.com>
2024-02-29 00:06:42 +0530

    to hosting commit 01

---
+++ src/Data/constants.js
@@ -0,0 +1,183 @@
+export const Bio = {
+    name: "Ashwani Gupta",
+    roles: [
+      "Full Stack Developer",
+      "Android Developer",
+      "Programmer",
+    ],
+    description:
+      "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
+    github: "https://github.com/Ashwanigupta9125",
+    resume:
+    "https://drive.google.com/file/d/1w9LHHA9TfyTHbLiFheWtjp2MzQo6P9vY/view?usp=sharing",
+    linkedin: "https://www.linkedin.com/in/ashwani-gupta-7b527a1b1/",
+    twitter: "https://twitter.com",
+    insta: "https://www.instagram.com",
+    facebook: "https://www.facebook.com",
+  };
+  
+  export const skills = [
+    {
+      title: "Frontend",
+      skills: [
+        {
+          name: "React Js",
+          image:
+            "data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9Ii0xMS41IC0xMC4yMzE3NCAyMyAyMC40NjM0OCI+CiAgPHRpdGxlPlJlYWN0IExvZ288L3RpdGxlPgogIDxjaXJjbGUgY3g9IjAiIGN5PSIwIiByPSIyLjA1IiBmaWxsPSIjNjFkYWZiIi8+CiAgPGcgc3Ryb2tlPSIjNjFkYWZiIiBzdHJva2Utd2lkdGg9IjEiIGZpbGw9Im5vbmUiPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIi8+CiAgICA8ZWxsaXBzZSByeD0iMTEiIHJ5PSI0LjIiIHRyYW5zZm9ybT0icm90YXRlKDYwKSIvPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIiB0cmFuc2Zvcm09InJvdGF0ZSgxMjApIi8+CiAgPC9nPgo8L3N2Zz4K",
+        },
+        
+        {
+          name: "HTML",
+          image: "https://www.w3.org/html/logo/badge/html5-badge-h-solo.png",
+        },
+        {
+          name: "CSS",
+          image:
+            "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/CSS3_logo_and_wordmark.svg/1452px-CSS3_logo_and_wordmark.svg.png",
+        },
+        {
+          name: "JavaScript",
+          image:
+            "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/JavaScript-logo.png/800px-JavaScript-logo.png",
+        },
+        {
+          name: "Bootstrap",
+          image:
+            "https://getbootstrap.com/docs/5.3/assets/brand/bootstrap-logo-shadow.png",
+        },
+
+      ],
+    },
+    {
+      title: "Backend",
+      skills: [
+        {
+          name: "Spring MVC",
+          image: "https://www.vectorlogo.zone/util/preview.html?image=/logos/springio/springio-icon.svg",
+        },
+        {
+          name: "Spring Boot",
+          image:"https://www.vectorlogo.zone/util/preview.html?image=/logos/springio/springio-icon.svg",
+        },
+        {
+          name: "Python",
+          image:
+            "https://raw.githubusercontent.com/devicons/devicon/master/icons/python/python-original.svg",
+        },
+        {
+          name: "MySQL",
+          image:
+            "https://raw.githubusercontent.com/devicons/devicon/master/icons/mysql/mysql-original-wordmark.svg",
+        },
+        {
+          name: "Firebase",
+          image: "https://www.vectorlogo.zone/logos/firebase/firebase-icon.svg",
+        },
+      ],
+    },
+    {
+      title: "Android",
+      skills: [
+        {
+          name: "Java",
+          image:
+            "https://raw.githubusercontent.com/devicons/devicon/master/icons/java/java-original.svg",
+        },
+        {
+          name: "XML",
+          image:
+            "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSBMw6_RdwKQ9bDFfnKDX1iwMl4bVJEvd9PP53XuIw&s",
+        },
+        {
+          name: "Android Studio",
+          image:
+            "https://developer.android.com/static/studio/images/new-studio-logo-1_1920.png",
+        },
+      ],
+    },
+    {
+      title: "Others",
+      skills: [
+        {
+          name: "Git",
+          image:
+            "https://camo.githubusercontent.com/fbfcb9e3dc648adc93bef37c718db16c52f617ad055a26de6dc3c21865c3321d/68747470733a2f2f7777772e766563746f726c6f676f2e7a6f6e652f6c6f676f732f6769742d73636d2f6769742d73636d2d69636f6e2e737667",
+        },
+        {
+          name: "GitHub",
+          image:
+            "https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png",
+        },
+        {
+          name: "VS Code",
+          image:
+            "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Visual_Studio_Code_1.35_icon.svg/512px-Visual_Studio_Code_1.35_icon.svg.png?20210804221519",
+        },
+        {
+          name: "Postman",
+          image:
+            "https://camo.githubusercontent.com/93b32389bf746009ca2370de7fe06c3b5146f4c99d99df65994f9ced0ba41685/68747470733a2f2f7777772e766563746f726c6f676f2e7a6f6e652f6c6f676f732f676574706f73746d616e2f676574706f73746d616e2d69636f6e2e737667",
+        },
+        
+      ],
+    },
+  ];
+  
+  export const experiences = [
+    {
+      id: 0,
+      img: "",
+      role: "Python Developer",
+      company: "Behind The Screens",
+      date: "apr 2021 - July 2021",
+      desc: "working as backend python developer and automation on selenium working",
+      skills: [
+             "Python","selenium"
+      ],
+    },
+    {
+      id: 0,
+      img: "",
+      role: "Frontend Engineer Intern",
+      company: "ConnectingDots Infotech",
+      date: "Oct 2021 - Dec 2021",
+      desc: "work as Android Developer work on pagination recyclerview.",
+      skills: [
+        "Java",
+        "Andriod studio",
+        "XMl",
+        "Firebase"
+      ],
+      doc: "",
+    },
+    {
+      id: 1,
+      img: "",
+      role: "Fullstack Java",
+      company: "Cognizant",
+      date: "Fed 2022 - presnt",
+      desc: "Built an employee management full stack web app used Docker and deployed on AWS ec2. I was the top performer in the program.",
+      skills: [
+        "ReactJS",
+        "HTML",
+        "CSS",
+        "JavaScript",
+        "Spring Boot",
+        "MongoDB",
+      ],
+    },
+  ];
+  
+  export const education = [
+    {
+      id: 0,
+      img: "",
+      school: "JAYPEE INSTITUTE OF INFORMATION TECHNOLOGY,Noida",
+      date: "Oct 2018 - Sep 2022",
+      grade: "7 CGPA",
+      desc: "a Bachelor's degree in ECE and Engineering at JAYPEE INSTITUTE OF INFORMATION TECHNOLOGY, Noida. ",
+      degree: "Bachelor of Technology - BTech, ECE",
+    },
+   
+  ];
+  
